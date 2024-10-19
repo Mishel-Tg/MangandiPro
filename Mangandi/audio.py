358421bbfe023c77aa0630416895f9c99713299d
Mishel <131391958+Mishel-Tg@users.noreply.github.com>
2024-10-19 16:31:39 +0530

    Create audio.py

---
+++ Mangandi/audio.py
@@ -0,0 +1,32 @@
+import requests
+from typing import Union
+
+class AudioUploader:
+    URL = "https://mangandi-2-0.onrender.com"
+
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+
+    def _read_file(self) -> bytes:
+        with open(self.file_path, "rb") as f:
+            return f.read()
+
+    def _upload(self) -> Union[str, dict]:
+        data = self._read_file()
+        files = {"file": data}
+        response = requests.post(
+            f"{self.URL}/aupload",
+            files=files
+        )
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return "not found"
+
+    def upload(self) -> str:
+        response_json = self._upload()
+        if "link" in response_json:
+            return response_json["link"]
+        else:
+            return "not found"
